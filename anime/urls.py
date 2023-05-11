c90a44f180d42814cd1395bc4b5ab4c7beb03a83
Antonio Fanoel <antonio.cabral@estudante.ifto.edu.br>
2023-05-11 19:02:44 -0300

    v3

--- anime/urls.py
+++ anime/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from animeapp.views import index
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', index, name='index'),
 ]
+
+
